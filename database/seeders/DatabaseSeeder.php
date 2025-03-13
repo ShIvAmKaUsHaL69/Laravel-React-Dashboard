b49c777d36c7c34a3807795e769b1eb6cfe6ec65
ShIvAmKaUsHaL69 <85203548+ShIvAmKaUsHaL69@users.noreply.github.com>
2025-03-13 23:43:11 +0530

    created databases for task and project

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,10 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Shivam',
+            'email' => 'shivam@example.com',
+            'password' => bcrypt('Shivam1234'),
+            'email_verified_at' => time(),
         ]);
     }
 }
